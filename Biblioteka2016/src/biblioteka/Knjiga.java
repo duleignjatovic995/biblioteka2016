cd5b9f5c12836ce1d8b40e6a1e8f296eb4f08f1f
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2016-03-08 13:49:47 +0100

    Dodati toString, equals i hashCode u Knjigu i Autora

--- Biblioteka2016/src/biblioteka/Knjiga.java
+++ Biblioteka2016/src/biblioteka/Knjiga.java
@@ -24,6 +24,11 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 	
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
 	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
@@ -41,6 +46,26 @@ public class Knjiga {
 	}
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn != other.isbn)
+			return false;
+		return true;
 	}
 	
 	
